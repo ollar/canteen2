f02f386763316f745d5d5477748421de9d29ef11
Oleg Larkin <Oleg Larkin@localhost>
2015-05-20 18:37:40 +0300

    [~] small fixes

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from flask.ext.login import UserMixin
 from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from werkzeug import generate_password_hash
+from werkzeug.security import gen_salt
 import datetime
 
 class User(Base, UserMixin):
@@ -70,3 +71,32 @@ class Meal(Base):
 
     def __repr__(self):
         return '<Meals {0}>'.format(self.title)
+
+
+class Token(Base):
+    __tablename__ = 'token'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
+    token = Column(String(40))
+    expires = Column(DateTime)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.token = self.generate_token()
+        self.expires = self.calc_expire_date()
+
+    @staticmethod
+    def generate_token():
+        return gen_salt(40)
+
+    @staticmethod
+    def calc_expire_date():
+        _date = datetime.datetime.utcnow() + datetime.timedelta(3)
+
+        return _date
+
+    def is_expired(self):
+        return self.expires < datetime.datetime.utcnow()
+
+    def __repr__(self):
+        return "<Token {0} for user {1}>".format(self.id, self.user_id)
