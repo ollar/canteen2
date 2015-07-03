df23a9e2b9067b92ee082f5911159c57a9571970
Oleg Larkin <Oleg Larkin@localhost>
2015-07-03 15:38:31 +0300

    [~] meal models price added.

--- main/models.py
+++ main/models.py
@@ -61,16 +61,20 @@ class Meal(Base):
     description = Column(String)
     category = Column(Integer)
     day_linked = Column(Integer)
+    source_price = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
     enabled = Column(Boolean, default=True)
     timestamp_created = Column(
         DateTime, default=datetime.datetime.utcnow(), nullable=False)
     timestamp_modified = Column(DateTime)
 
-    def __init__(self, title, description, category, day_linked, enabled=1, *args, **kwargs):
+    def __init__(self, title, description, category, day_linked, enabled=1, source_price=0, price=0, *args, **kwargs):
         self.title = title
         self.description = description
         self.category = category
         self.day_linked = day_linked
+        self.source_price = source_price
+        self.price = price
         self.enabled = enabled
         self.timestamp_modified = datetime.datetime.utcnow()
 
