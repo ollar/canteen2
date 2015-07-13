c6d7cbe6547376b51883bca7579ea503837edf17
Oleg Larkin <Oleg Larkin@localhost>
2015-07-13 18:02:58 +0300

    demo data added

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 from flask.ext.script import Manager, Command, Option
 from main.main import app
 
+from sqlalchemy import create_engine
+from main.database import Base, db_session
+from main.Flask_configs import Config, DevConfig
+from main.models import User, Order, Meal
+import random
+import requests
+import datetime
+import json
+
 manager = Manager(app)
 
 
@@ -20,10 +29,6 @@ class InitDB(Command):
     )
 
     def __init__(self):
-        from sqlalchemy import create_engine
-        from main.database import Base
-        from main.Flask_configs import Config, DevConfig
-
         self.prod_sql_url, self.dev_sql_url = Config.SQLALCHEMY_DATABASE_URI, DevConfig.SQLALCHEMY_DATABASE_URI
         self.create_engine = create_engine
         self.Base = Base
@@ -39,8 +44,103 @@ class InitDB(Command):
         print("Database created.")
 
 
-manager.add_command('hello', Hello())
-manager.add_command('update_db', InitDB())
+class PopulateMeals(Command):
+    """
+    Creates fake meals.
+    """
+    def __init__(self):
+        pass
+
+    def run(self):
+        with open('./demo_meals.json', 'r') as f:
+            meals = json.load(f)
+
+            for meal in meals:
+                new_meal = Meal(title=meal.get('title'),
+                                description=meal.get('description'),
+                                category=meal.get('category'),
+                                day_linked=meal.get('day_linked'),
+                                source_price=meal.get('source_price'),
+                                price=meal.get('price'),
+                                enabled=meal.get('enabled'),
+                                timestamp_modified=datetime.datetime.utcnow())
+
+                db_session.add(new_meal)
+                db_session.commit()
+
+                print('Created meal:', new_meal)
+
+
+
+class PopulateUsers(Command):
+    """
+    Creates fake users.
+    """
+    option_list = (
+        Option('--number', '-n', dest='num', default=20),
+    )
+    def __init__(self):
+        pass
+
+    def run(self, num):
+        for i in range(num):
+            with open('./words', 'r') as f:
+                word = random.choice(f.read().splitlines())
+
+            new_user = User(real_name=word,
+                            username=word,
+                            password=word)
+
+            db_session.add(new_user)
+            db_session.commit()
+
+            print('Created user:', new_user)
+
+
+class PopulateOrders(Command):
+    """
+    Creates fake orders.
+    """
+    option_list = (
+        Option('--number', '-n', dest='num', default=200),
+    )
+    def __init__(self):
+            self.users = db_session.query(User).all()
+            self.meals = db_session.query(Meal).all()
+
+    def _get_day_meals(self, order_date):
+        _meals = [meal for meal in self.meals if meal.day_linked == order_date.weekday()]
+
+        if not _meals:
+            return self._get_day_meals(order_date + datetime.timedelta(days=1))
+
+        return _meals
+
+
+    def run(self, num):
+        for user in self.users:
+            for i in range(num):
+                order_date = datetime.date.today() + datetime.timedelta(days=random.randint(-100,20))
+
+                _meals = self._get_day_meals(order_date)
+
+                new_order = Order(order_date=order_date,
+                                  meal_id=random.choice(_meals).id,
+                                  user_id=user.id,
+                                  quantity=random.randint(1,10))
+
+                db_session.add(new_order)
+                db_session.commit()
+
+                print('Created order:', new_order)
+
+
+
+manager.add_command('hello', Hello)
+manager.add_command('update_db', InitDB)
+manager.add_command('popme', PopulateMeals)
+manager.add_command('popus', PopulateUsers)
+manager.add_command('popor', PopulateOrders)
 
 if __name__ == '__main__':
     manager.run()
